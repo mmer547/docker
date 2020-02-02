551d6801161168c44b773d21cc5a49f4cbf5b192
mmer547 <hammamania@gmail.com>
2020-02-02 23:27:19 +0900

    First Commit

---
+++ openfoam7_container/buildOF.bat
@@ -0,0 +1,5 @@
+REM docker build -t myopenfoam . --no-cache
+REM docker build -t myopenfoam .
+REM docker create --name myopenfoam myopenfoam
+docker-compose up
+pause
---
+++ openfoam7_container/startOF.bat
@@ -0,0 +1,4 @@
+REM docker run -it myopenfoam
+REM docker run -it myopenfoam -v %USERPROFILE%:/mnt/windows -e DISPLAY=192.168.56.1:0.0
+docker-compose run myopenfoam
+pause
